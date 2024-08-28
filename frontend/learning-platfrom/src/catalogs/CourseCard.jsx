a7e992ab0647d7403dd735eb223e6646b3709651
Azad <arambagh53@gmail.com>
2024-08-28 10:30:49 +0530

    star Rating update

--- frontend/learning-platfrom/src/catalogs/CourseCard.jsx
+++ frontend/learning-platfrom/src/catalogs/CourseCard.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-
+import StarRatings from "react-star-ratings";
+import RatingStars from "./RatingStars";
 const CourseCard = ({ courseDetails }) => {
   console.log("Course Details is : ", courseDetails);
-  
-  const { thumbnail , courseName , price , instructor } = courseDetails;
-  const { firstName , lastName }  = instructor;
+
+  const { thumbnail, courseName, price, instructor } = courseDetails;
+  const { firstName, lastName } = instructor;
   return (
-    <div className=" w-[300px] font-inter scale-[0.9] hover:scale-[1] transition-all duration-900 border border-pure-greys-500 rounded-md">
-      <img src={thumbnail} alt='courseImage' loading="lazy" className=" w-full h-[160px] object-cover rounded-t-md"/>
+    <div className=" w-[300px] h-fit font-inter scale-[0.95] hover:scale-[1] transition-all duration-900 border border-richblack-600 rounded-md">
+      <img
+        src={thumbnail}
+        alt="courseImage"
+        loading="lazy"
+        className=" w-full h-[160px] object-cover rounded-t-md"
+      />
       <div className=" w-full min-h-36 p-2 bg-richblack-800 rounded-b-md flex flex-col gap-2">
-          <p className=" font-semibold text-xl text-pure-greys-50">{courseName}</p>
-          <p className="text-[12px] md:text-lg text-richblack-5">By <span className="text-yellow-50">{firstName + " " + lastName}</span></p>
+        <p className=" font-semibold text-xl text-pure-greys-50">
+          {courseName}
+        </p>
+        <p className="text-[12px] md:text-lg text-richblack-5">
+          By{" "}
+          <span className="text-yellow-50">{firstName + " " + lastName}</span>
+        </p>
+        <div className="w-full flex gap-x-3 item-center">
+        <div className=" text-yellow-50 text-lg font-semibold ">3.5</div>
+          <RatingStars rating_cnt={3.5}/>
+          <div className=" text-lg text-pure-greys-25 ">7 Ratings</div>
+        </div>
+        <p className="text-sm md:text-xl text-richblack-5">Rs. 599</p>
       </div>
     </div>
   );
---
+++ frontend/learning-platfrom/src/catalogs/RatingStars.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react"
+import {
+  TiStarFullOutline,
+  TiStarHalfOutline,
+  TiStarOutline,
+} from "react-icons/ti"
+
+const RatingStars = ({rating_cnt}) => {
+    const [allStarCount , setAllStarCnt ] = useState({
+        fullStar : 0 , 
+        halfStar : 0 , 
+        emptyStar : 0 
+    })
+    useEffect(() => {
+        let fullStar = Math.floor(rating_cnt) || 0;
+        let halfStar = Number.isInteger(rating_cnt) ? 0 : 1;
+        let emptyStar = Number.isInteger(rating_cnt) ? 5 - fullStar : 4 - fullStar;
+        
+        setAllStarCnt({
+            emptyStar , halfStar  , fullStar
+        })
+    } , [rating_cnt])
+  return (
+    <div className="flex gap-1 text-yellow-100">
+        {
+            Array.from({length : allStarCount.fullStar} , (_ , i) => {
+                return <TiStarFullOutline key={i} size={20}/>
+            })
+        }
+        {
+            Array.from({length : allStarCount.halfStar} , (_ , i) => {
+                return <TiStarHalfOutline key={i} size={20}/>
+            })
+        }
+        {
+            Array.from({length : allStarCount.emptyStar} , (_ , i) => {
+                return <TiStarOutline key={i} size={20}/>
+            })
+        }
+    </div>
+  )
+}
+
+export default RatingStars
