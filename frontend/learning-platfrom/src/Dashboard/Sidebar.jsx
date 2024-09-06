1cd57a808fb16a9c1b82902d29dc2eab24ccd5cc
Azad <arambagh53@gmail.com>
2024-09-06 13:53:17 +0530

    slider phone updated

--- frontend/learning-platfrom/src/Dashboard/Sidebar.jsx
+++ frontend/learning-platfrom/src/Dashboard/Sidebar.jsx
@@ -20,7 +20,8 @@ const Sidebar = () => {
       setLogoutModal(!logoutModal);
     }
   return (
-    <div className=' relative w-[230px] min-h-screen bg-richblack-700 flex flex-col items-center justify-start gap-y-3'>
+  <>
+    <div className='lg:visible hidden relative w-[230px] min-h-screen bg-richblack-700 lg:flex flex-col items-center justify-start gap-y-3'>
     {
       sidebarLinks.map((ele , index) => {
         if(ele.type && user.accountType !== ele.type) return null;
@@ -61,6 +62,36 @@ const Sidebar = () => {
       }
      
     </div>
+    
+
+     <div className=' lg:hidden z-10 visible fixed  bottom-0 left-0 right-0 w-full h-12 bg-richblack-900 p-2 flex items-center 
+     justify-between border-t border-pure-greys-400 overflow-y-hidden'>
+     {
+      sidebarLinks.map((ele , index) => {
+        if(ele.type && user.accountType !== ele.type) return null;
+        const IconComponet = ele.icon;
+        return (
+          <NavLink to={ele.path} className="w-full" key={index}>
+            <div key={ele.id} className = {`${matchRoute(ele.path) ? " bg-yellow-700 border-l-4 border-l-yellow-25" : " bg-opacity-0"} flex gap-2 w-full
+                p-3 cursor-pointer text-sm font-inter text-pure-greys-200 items-center justify-center
+            `}>
+             <IconComponet className=' w-[1.12rem] h-[1.12rem] sm:w-8 sm:h-8' />
+              <p className=' hidden md:block'>{ele.name}</p>
+            </div>
+          </NavLink>
+          
+        )
+      })  
+    }
+    <NavLink to={"/dashboard/default/setting"} className="w-full">
+        <div key={"Setting"} className = {`${matchRoute("/dashboard/default/setting") ? " bg-yellow-700 border-l-4 border-l-yellow-25" : " bg-opacity-0"} flex gap-2 w-full
+            p-3 cursor-pointer text-sm font-inter text-pure-greys-200 items-center justify-center `}>
+             <IoIosSettings className=' w-[1.12rem] h-[1.12rem] sm:w-8 sm:h-8' />
+              <p className='hidden md:block'>Settings</p>
+        </div>
+      </NavLink>
+    </div>
+</>
   )
 }
 
