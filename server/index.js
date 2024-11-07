70b49637b6366aa4f871a72fc3782d231f76346e
Azad <arambagh53@gmail.com>
2024-11-07 15:21:11 +0530

    cors origin Changed

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:3000', credentials: true 
+    origin: '*', credentials: true 
 }))
 
 app.use(fileUpload({
