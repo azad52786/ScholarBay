fef6f186ffbba8101f1ad258849d87bfea847638
Kaji Azad Ali <arambagh53@gmail.com>
2026-07-20 06:01:50 +0530

    fix: Ensure MAIL_PORT is parsed as an integer in mailSender function

--- server/utils/mailSend.js
+++ server/utils/mailSend.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body, attachmentBuffer = null, attachmentName = null) => {
     let transporter = nodemailer.createTransport({
         host : process.env.MAIL_HOST, 
+        port: parseInt(process.env.MAIL_PORT),
         auth : {
             user : process.env.MAIL_USER,
             pass :  process.env.MAIL_PASS,
